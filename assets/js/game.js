3a6d3918971274e1319d768b318feeb6c4baa55c
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2021-12-15 01:17:08 -0600

    updaed game over code statement

--- assets/js/game.js
+++ assets/js/game.js
@@ -98,22 +98,15 @@ var fight = function (enemyNames) {
     }    
 }
 
-for(var i = 0; i < enemyNames.length; i++) {
+
+
+
+ for(var i = 0; i < enemyNames.length; i++) {
     
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-    }
-
-
-    // NEED TO MAKE THE GAME OVER CODE ALERT SHOW UP PROPERLY
-
-
-
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
+    
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
 
@@ -126,5 +119,8 @@ for(var i = 0; i < enemyNames.length; i++) {
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
     //fight(enemyNames[i]);
- } 
-//fight();
+ } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
+ }
